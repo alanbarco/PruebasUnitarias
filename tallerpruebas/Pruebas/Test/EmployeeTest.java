6b54c7afab09409adae61f9eb1d6180117e452d8
VivianaVelasco <vivavela@espol.edu.ec>
2021-08-23 10:52:05 -0500

    Agregando tres test...

--- tallerpruebas/Pruebas/Test/EmployeeTest.java
+++ tallerpruebas/Pruebas/Test/EmployeeTest.java
@@ -29,5 +29,43 @@ public class EmployeeTest {
 		 float salarioEsperado = 100.35f;
 		 assertEquals(salarioactual, salarioEsperado, 0.1);
 	 }
+	
+
+
+	/* Calcula el salario de un Supervisor cuando el salario base es de $200
+	 * dolares americanos y el mes es par. Es decir 23 de Agosto = 8 PAR
+	 */
+	@Test
+	 public void csWorkerUSDParTest() {
+		 Employee e = new Employee(200f, "USD", 1f, EmployeeType.Worker);
+		 float salarioactual = e.cs();
+		 float salarioEsperado = 200f;
+		 assertEquals(salarioactual, salarioEsperado, 0.1);
+	 }
+	
+	/*
+	 * Calcula el salario de un Employee Worker cuando su salario base es de 800 EUR la moneda 
+	 * y el mes es par. Se prueba con agosto Agosto = 8 PAR
+	 */
+	 @Test
+	 public void csWorkerEURParTest() {
+		 Employee e = new Employee(800f, "EUR", 1f, EmployeeType.Worker);
+		 float salarioactual = e.cs();
+		 float salarioEsperado = 760f;
+		 assertEquals(salarioactual, salarioEsperado, 0.1); 
+	 }
+	 
+	 /*
+	 * Calcula el salario de un Employee Supervisor cuando su salario base es de 1000 EUR la moneda 
+	 * y el mes es impar. Se prueba con Septiembre = 9 IMPAR
+	 */
+	 @Test
+	 public void csSupervisorEURImparTest() {
+		 Employee e = new Employee(1000f, "EUR", 1f, EmployeeType.Supervisor);
+		 float salarioactual = e.cs();
+		 System.out.println(salarioactual);
+		 float salarioEsperado = 1014.6833f;
+		 assertEquals(salarioactual, salarioEsperado, 0.1);
+	 }
 
 }
